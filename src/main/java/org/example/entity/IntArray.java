1c8b30f842ec10cc3976cecb2fbf3f4c3bfba882
unf0r9et <unforgettableunf2006@gmail.com>
2026-05-29 02:31:22 +0300

    feat: added entity -> IntArray

---
+++ src/main/java/org/example/entity/IntArray.java
@@ -0,0 +1,41 @@
+package org.example.entity;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class IntArray {
+
+    private int[] array;
+
+    IntArray() {};
+    IntArray(int[] array) {
+        this.array = array.clone();
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
+    public void setArray(int[] array) {
+        this.array = array.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        IntArray intArray = (IntArray) o;
+        return Arrays.equals(array, intArray.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
+
+    @Override
+    public String toString() {
+        return "IntArray{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+}
