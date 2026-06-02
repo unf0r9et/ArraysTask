17ea09c34a12fd058bf18a0328efbc4f7fc3dc39
unf0r9et <unforgettableunf2006@gmail.com>
2026-06-02 16:55:16 +0300

    feat: added ArrayReaderException

---
+++ src/main/java/org/example/exception/ArrayReaderException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class ArrayReaderException extends RuntimeException {
+    public ArrayReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
