a18bc0db88f5e07d17cd59d1c324d1f3570fcad9
unf0r9et <unforgettableunf2006@gmail.com>
2026-06-02 16:54:01 +0300

    feat: added SortService -> interface & implementation

---
+++ src/main/java/org/example/service/SortService.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+import org.example.entity.IntArray;
+
+public interface SortService {
+    void bubbleSort(IntArray intArray);
+    void selectionSort(IntArray intArray);
+}
---
+++ src/main/java/org/example/service/impl/SortServiceImpl.java
@@ -0,0 +1,41 @@
+package org.example.service.impl;
+
+import org.example.entity.IntArray;
+import org.example.service.SortService;
+
+public class SortServiceImpl implements SortService {
+
+    @Override
+    public void bubbleSort(IntArray intArray) {
+        int[] array = intArray.getArray();
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void selectionSort(IntArray intArray) {
+        int[] array = intArray.getArray();
+        int length = array.length;
+
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+}
