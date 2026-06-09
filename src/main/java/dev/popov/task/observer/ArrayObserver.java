fca8571b1573038a489e538e1aee100425bb40ff
unf0r9et <unforgettableunf2006@gmail.com>
2026-06-09 17:07:21 +0300

    feat: added ArrayObserver

---
+++ src/main/java/dev/popov/task/observer/ArrayObserver.java
@@ -0,0 +1,7 @@
+package dev.popov.task.observer;
+
+import dev.popov.task.entity.IntArray;
+
+public interface ArrayObserver {
+    void update(IntArray entity);
+}
---
+++ src/main/java/dev/popov/task/observer/impl/ArrayObserverImpl.java
@@ -0,0 +1,37 @@
+package dev.popov.task.observer.impl;
+
+import dev.popov.task.entity.ArrayStatistics;
+import dev.popov.task.entity.IntArray;
+import dev.popov.task.observer.ArrayObserver;
+import dev.popov.task.warehouse.ArrayWarehouse;
+
+public class ArrayObserverImpl implements ArrayObserver {
+
+    private final ArrayWarehouse warehouse;
+
+    public ArrayObserverImpl(ArrayWarehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void update(IntArray entity) {
+        ArrayStatistics statistic = calculate(entity.getArray());
+        warehouse.put(entity.getId(), statistic);
+    }
+
+    private ArrayStatistics calculate(int[] array){
+        int sum = 0;
+        int min = array[0];
+        int max = array[0];
+
+        for (int d : array) {
+            sum += d;
+            if (d < min) min = d;
+            if (d > max) max = d;
+        }
+
+        double average = (double) sum / array.length;
+
+        return new ArrayStatistics(min, max, sum, average);
+    }
+}
